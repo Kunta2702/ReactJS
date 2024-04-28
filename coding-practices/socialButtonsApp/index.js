f929b4e32899db60cf6cd4f2d13e41e140638f58
moulikakunta23@gmail.com <Kunta Moulika>
2024-04-28 06:03:15 +0000

    Intro-To-ReactjS coding practice2 socialButtons1

---
+++ coding-practices/socialButtonsApp/index.js
@@ -0,0 +1,12 @@
+const Button = props => {
+  //  Write your code here.
+}
+
+const element = (
+  //  Write your code here.
+  <div classname="bg-container">
+    
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
